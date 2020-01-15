1276e5aaf48afd3597f8de4f966d314072a6aecc
OLX - Gaston D Ippoliti <gaston.dippoliti@olx.com>
2020-01-14 22:42:58 -0300

    Delete Main Storyboard & re organize project
    * Delete Main Storyboard
    * Create Application and Supporting Files folders. Re organize files into them.

---
+++ Deviget-Challenge/Deviget-ChallengeTests/Deviget_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Deviget_ChallengeTests.swift
+//  Deviget-ChallengeTests
+//
+//  Created by OLX - Gaston D Ippoliti on 14/01/2020.
+//  Copyright © 2020 GastonDippoliti. All rights reserved.
+//
+
+import XCTest
+@testable import Deviget_Challenge
+
+class Deviget_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
